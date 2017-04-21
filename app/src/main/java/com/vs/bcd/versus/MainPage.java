5e6b40128fe097a1cba8b6ef43bae8f61204e44a
dlee121 <dlee121@illinois.edu>
2017-04-20 23:17:42 -0500

    Changed Profile Page to Main Page and edited XMLs accordingly

--- app/src/main/java/com/vs/bcd/versus/MainPage.java
+++ app/src/main/java/com/vs/bcd/versus/MainPage.java
@@ -1,18 +1,15 @@
 package com.vs.bcd.versus;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class ProfilePage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.activity_main_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
