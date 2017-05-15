abb16e83b1619702913e029348dc9e3b8cdca3a4
dlee121 <dlee121@illinois.edu>
2017-05-15 00:42:12 -0500

    Newsfeed works as it should now. Main UI loads right away, then the actual feed loads once data from query is completely loaded.

--- app/src/main/java/com/vs/bcd/versus/Tab1Newsfeed.java
+++ app/src/main/java/com/vs/bcd/versus/Tab1Newsfeed.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.os.Handler;
 import android.widget.Toast;
+import android.app.Activity;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -30,68 +31,16 @@ public class Tab1Newsfeed extends Fragment{
     private List<Post> posts;
     private MyAdapter myAdapter;
     private boolean dataLoaded = false;
+    private View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab1newsfeed, container, false);
+        rootView = inflater.inflate(R.layout.tab1newsfeed, container, false);
         //TODO: need to add categories. maybe a separate categories table where post IDs have rows of categories they are linked with
         //TODO: this is where I need to get data from database and construct list of Post objects
         //TODO: create, at the right location, list of constant enumeration to represent categories. probably at post creation page, which is for now replaced by sample data creation below
         //for now, create sample data
-        ddbQuery();
-        //TODO: replace the line below with actual thread.join instead of this while loop
-        while (!dataLoaded){};
 
-
-        //find view by id and attaching adapter for the RecyclerView
-        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        //this is where the list is passed on to adapter
-        myAdapter = new MyAdapter(recyclerView, posts, getActivity());
-        recyclerView.setAdapter(myAdapter);
-
-        //set load more listener for the RecyclerView adapter
-        myAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
-            @Override
-            public void onLoadMore() {
-
-                if (posts.size() <= 3) {
-                    /*
-                    posts.add(null);
-                    myAdapter.notifyItemInserted(posts.size() - 1);
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            posts.remove(posts.size() - 1);
-                            myAdapter.notifyItemRemoved(posts.size());
-
-                            //Generating more data
-                            int index = posts.size();
-                            int end = index + 10;
-                            for (int i = index; i < end; i++) {
-                                Contact contact = new Contact();
-                                contact.setEmail("DevExchanges" + i + "@gmail.com");
-                                posts.add(contact);
-                            }
-                            myAdapter.notifyDataSetChanged();
-                            myAdapter.setLoaded();
-                        }
-                    }, 1500);
-                    */
-                } else {
-                    Toast.makeText(getActivity(), "Loading data completed", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-
-        return rootView;
-    }
-
-
-    private void ddbQuery(){
         // Initialize the Amazon Cognito credentials provider
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                 getActivity().getApplicationContext(),
@@ -114,12 +63,60 @@ public class Tab1Newsfeed extends Fragment{
                     Post element = it.next();
                     posts.add(element);
                 }
-                dataLoaded = true;
+                //run UI updates on UI Thread
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //find view by id and attaching adapter for the RecyclerView
+                        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                        //this is where the list is passed on to adapter
+                        myAdapter = new MyAdapter(recyclerView, posts, getActivity());
+                        recyclerView.setAdapter(myAdapter);
+
+                        //set load more listener for the RecyclerView adapter
+                        myAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+                            @Override
+                            public void onLoadMore() {
+
+                                if (posts.size() <= 3) {
+                    /*
+                    posts.add(null);
+                    myAdapter.notifyItemInserted(posts.size() - 1);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            posts.remove(posts.size() - 1);
+                            myAdapter.notifyItemRemoved(posts.size());
+
+                            //Generating more data
+                            int index = posts.size();
+                            int end = index + 10;
+                            for (int i = index; i < end; i++) {
+                                Contact contact = new Contact();
+                                contact.setEmail("DevExchanges" + i + "@gmail.com");
+                                posts.add(contact);
+                            }
+                            myAdapter.notifyDataSetChanged();
+                            myAdapter.setLoaded();
+                        }
+                    }, 1500);
+                    */
+                                } else {
+                                    Toast.makeText(getActivity(), "Loading data completed", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+
+                    }
+                });
             }
         };
         Thread mythread = new Thread(runnable);
         mythread.start();
-
+        return rootView;
     }
-
 }
