c6c9217b37ef16e7d13140241142df62d48d0d15
dlee121 <dlee121@illinois.edu>
2017-06-08 20:38:26 -0500

    PostPage started

---
+++ app/src/main/java/com/vs/bcd/versus/fragment/PostPage.java
@@ -0,0 +1,33 @@
+package com.vs.bcd.versus.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vs.bcd.versus.R;
+
+/**
+ * Created by dlee on 6/7/17.
+ */
+
+public class PostPage extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.post_page, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            Log.d("VISIBLE", "POST PAGE VISIBLE");
+        }
+        else
+            Log.d("VISIBLE", "POST PAGE GONE");
+    }
+}
